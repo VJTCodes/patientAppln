ba705b31cf03b78a86004ef162bac2098bc04dab
VJTCodes <50662389+VJTCodes@users.noreply.github.com>
2025-01-06 21:51:37 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+name: any;
+password: any;
+constructor(private router:Router){
+
+}
+
+login(){
+  if(this.name?.length > 0 && this.password?.length >0){
+    this.router.navigate(['/dashboard'])
+  }
+  else{
+    alert("Please provide valid user name and password")
+  }
+}
 
 }
